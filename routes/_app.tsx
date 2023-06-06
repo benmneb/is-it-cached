54052191ab60a8d50b474a01d2c493d61712f22e
benmneb <62878664+benmneb@users.noreply.github.com>
2023-06-06 08:31:39 +0800

    my sixth commit 🫢

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -12,9 +12,10 @@ export default function App({ Component }: AppProps) {
 				/>
 				<script
 					async
+					defer
 					src="https://umamalytics.vercel.app/script.js"
 					data-website-id="340fbcac-cb8a-4677-856d-50899d0f36ed"
-					data-host-url="isitcached.com,isitcached.deno.dev"
+					data-domains="isitcached.com,isitcached.deno.dev"
 				></script>
 			</Head>
 			<body class="bg-neutral-50 dark:bg-neutral-800 text-neutral-800 dark:text-neutral-50 text-xl sm:text-2xl md:text-3xl lg:text-4xl max-w-full lg:max-w-screen-lg mx-auto">

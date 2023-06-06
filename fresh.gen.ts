c37d9df61cef9e166eb8076f441f7281a46dd1fb
benmneb <62878664+benmneb@users.noreply.github.com>
2023-06-06 22:02:14 +0800

    my eighth commit 💅

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,6 +6,7 @@ import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_app.tsx";
 import * as $1 from "./routes/index.tsx";
 import * as $$0 from "./islands/count-down.tsx";
+import * as $$1 from "./islands/form-content.tsx";
 
 const manifest = {
   routes: {
@@ -14,6 +15,7 @@ const manifest = {
   },
   islands: {
     "./islands/count-down.tsx": $$0,
+    "./islands/form-content.tsx": $$1,
   },
   baseUrl: import.meta.url,
   config,
